579d54e6e6b81cce2e9d9b349c4779b5614d8381
Eduard Kracmar <kracmar@dannax.sk>
2010-01-21 11:33:47 +0100

    Added complete PHPDocs for DibiOrmController and reordering methods and properties

--- DibiOrmController.php
+++ DibiOrmController.php
@@ -1,69 +1,173 @@
 <?php
+
 /**
- * controler
+ * DibiOrm - Object-relational mapping based on David Grudl's dibi
  *
- * @author kraken
+ * @copyright  Copyright (c) 2010 Eduard 'edke' Kracmar
+ * @license    no license set at this point
+ * @link       http://dibiorm.local :-)
+ * @category   DibiOrm
+ * @package    DibiOrm
  */
-class DibiOrmController
+
+
+/**
+ * DibiOrmController
+ *
+ * Enviroment for DibiOrm:
+ *  - database operations
+ *  - models collection
+ *  - configuration options
+ *  - robot loader
+ *  - driver and dibi connection
+ *  - cache
+ *  - sql buffer for pretty examples
+ *
+ *
+ * @final
+ * @copyright Copyright (c) 2010 Eduard 'edke' Kracmar
+ * @package DibiOrm
+ */
+
+final class DibiOrmController
 {
-    protected static $models= null;
+
 
     /**
-     * @var DibiConnection
-     */
-    protected static $connection;
-
-    /**
-     * @var DibiOrmPostgreDriver
-     */
-    protected static $driver;
-
-    protected static $sqlBuffer= array();
-
-    protected static $robot;
-
-    /**
-     * @var boolean
-     */
-    protected static $modelCaching= true;
-
-    /**
+     * Instance of cache used by models
      * @var Cache
      */
     protected static $cache;
 
-    public static function getModels()
-    {
-	if ( is_null(self::$models))
-	{
-	    self::$models= self::getModelLoader()->getModels();
-	}
-	return self::$models;
-    }
-
-    public static function getModelMtime($model)
-    {
-	$key= strtolower(get_class($model));
-	return self::getModelLoader()->getModelMtime($key);
-    }
 
     /**
-     * @return DibiOrmModelLoader
+     * Instance of dibi connection used by models and controler
+     * @var DibiConnection
      */
-    public static function getModelLoader()
+    protected static $connection;
+
+
+    /**
+     * Instance of DibiDriver
+     * @var DibiOrmPostgreDriver
+     */
+    protected static $driver;
+
+
+    /**
+     * Collection of all application models
+     * @var array
+     */
+    protected static $models= null;
+
+
+    /**
+     * Instance of DibiOrmModelLoader
+     * @var DibiOrmModelLoader
+     */
+    protected static $robot;
+
+
+    /**
+     * Array of executed sql queries, used for prettier examples
+     * @var array
+     */
+    protected static $sqlBuffer= array();
+
+
+    /**
+     * Controls whether model uses cache or not
+     * @var boolean
+     */
+    protected static $modelCaching= true;
+
+
+    /**
+     * Adds sql query to buffer
+     * @param string $sql
+     */
+    public static function addSql($sql)
     {
-	if ( is_null(self::$robot))
-	{
-	    self::$robot= new DibiOrmModelLoader();
-	    self::$robot->addDirectory(APP_DIR);
-	    self::$robot->rebuild();
-	}
-	return self::$robot;
+	self::$sqlBuffer[]= $sql;
     }
 
 
     /**
-     * Vytvori/vrat instanciu pre Cache so storage MemcachedStorage()
+     * Models with dependancy defined are moved before its dependents
+     * @param array $list
+     */
+    protected static function dependancySort( &$list)
+    {
+	$_list= $list;
+	foreach($_list as $item)
+	{
+	    $min= null;
+	    foreach($item->getDependents() as $dependent)
+	    {
+		$min= ( is_null($min )) ? array_search($dependent, $_list) : min($min, array_search($dependent, $_list));
+	    }
+	    if (is_integer($min))
+	    {
+		if ( array_search($item, $_list) > $min)
+		{
+		    unset($list[array_search($item,$list)]);
+		    $list= self::insertArrayIndex($list, $item, $min);
+		}
+	    }
+	}
+    }
+
+
+    /**
+     * Reversed sort of dependancySort method
+     * @param array $list
+     */
+    protected static function dependancySortReverse( &$list)
+    {
+	self::dependancySort($list);
+	$list= array_reverse($list);
+    }
+
+
+    /**
+     * Disables model caching
+     */
+    public static function disableModelCaching()
+    {
+	self::$modelCaching= false;
+    }
+
+
+    /**
+     * Enables model caching
+     */
+    public static function enableModelCaching()
+    {
+	self::$modelCaching= true;
+    }
+
+
+    /**
+     * Executes sql query in transaction
+     * @param string $sql
+     */
+    protected static function execute($sql)
+    {
+	self::getConnection()->begin();
+	self::getConnection()->query($sql);
+	self::getConnection()->commit();
+    }
+
+
+    /**
+     * Getter for Cache instance
+     *
+     * Creates instance if called for the first time
+     * Creates MemcachedStorage if extension memcache exists
+     * Otherwise FileStorage Cache is created
+     *
+     * Triggers notice if config variable advertisememcached in dibiorm block is not set to false
+     *
      * @return Cache
      */
     public static function getCache()
@@ -76,7 +180,8 @@ class DibiOrmController
 		self::$cache= new Cache(new MemcachedStorage($config->memcache_host, $config->memcache_port, $config->cache_prefix));
 		$namespace= self::$cache;
 		$namespace['test']= true;
-		if ( $namespace['test'] === true) {
+		if ( $namespace['test'] === true)
+		{
 		    return self::$cache;
 		}
 	    }
@@ -89,7 +194,22 @@ class DibiOrmController
 
 
     /**
-     * @return DibiOrmPostgreDriver
+     * Getter for dibi connection
+     * @return DibiConnection
+     */
+    public static function getConnection()
+    {
+	if ( !self::$connection)
+	{
+	    self::$connection= dibi::getConnection();
+	}
+	return self::$connection;
+    }
+
+
+    /**
+     * Getter for DibiOrm driver
+     * @return DibiOrmDriver
      */
     public static function getDriver()
     {
@@ -106,18 +226,148 @@ class DibiOrmController
 	return self::$driver;
     }
 
+
     /**
-     * @return DibiConnection
+     * Getter for DibiOrmModelLoader
+     *
+     * Creates instance if called for the first time and loader is rebuilded
+     * @return DibiOrmModelLoader
      */
-    public static function getConnection()
+    public static function getModelLoader()
     {
-	if ( !self::$connection)
+	if ( is_null(self::$robot))
 	{
-	    self::$connection= dibi::getConnection();
+	    self::$robot= new DibiOrmModelLoader();
+	    self::$robot->addDirectory(APP_DIR);
+	    self::$robot->rebuild();
 	}
-	return self::$connection;
+	return self::$robot;
     }
 
+
+    /**
+     * Getter for model's definition class file mtime
+     * @param DibiOrm $model
+     * @return integer
+     */
+    public static function getModelMtime($model)
+    {
+	$key= strtolower(get_class($model));
+	return self::getModelLoader()->getModelMtime($key);
+    }
+
+
+    /**
+     * Getter for all models in application
+     * @return array
+     */
+    public static function getModels()
+    {
+	if ( is_null(self::$models))
+	{
+	    self::$models= self::getModelLoader()->getModels();
+	}
+	return self::$models;
+    }
+
+
+    /**
+     * Getter and cleaner for all sql queries in buffer
+     * @return array|boolean
+     */
+    public static function getSqlBufferAndClear()
+    {
+	$buffer= self::$sqlBuffer;
+	self::$sqlBuffer= array();
+	return (empty($buffer)) ? false : $buffer;
+    }
+
+
+    /**
+     * Inserts element at $index of $array
+     * @param array $array
+     * @param mixed $new_element
+     * @param integer $index
+     * @return array
+     */
+    protected static function insertArrayIndex($array, $new_element, $index)
+    {
+	$start = array_slice($array, 0, $index);
+	$end = array_slice($array, $index);
+	$start[] = $new_element;
+	return array_merge($start, $end);
+    }
+
+
+    /**
+     * Runs query and inserts it's sql code in buffer
+     */
+    public static function queryAndLog()
+    {
+	$args= func_get_args();
+	self::getConnection()->query($args);
+	self::addSql(dibi::$sql);
+    }
+
+
+    /**
+     * Models operation for showing database structure for defined models
+     * @return string
+     */
+    public static function sqlall()
+    {
+	$sql= null;
+	foreach( self::getModels() as $model)
+	{
+	    $sql .= $model->getDriver()->createTable($model);
+	}
+	return $sql;
+    }
+
+
+    /**
+     * Models operation for clearing database structure for defined models
+     *
+     * If confirm is set, sql code will be executed
+     *
+     * @param boolean> $confirm
+     * @return string
+     */
+    public static function sqlclear($confirm)
+    {
+	$sql= null;
+	$models = array();
+
+	foreach( self::getModels() as $model)
+	{
+	    if ( $model->getConnection()->getDatabaseInfo()->hasTable($model->getTableName()) )
+	    {
+		$models[]= $model;
+	    }
+	}
+	self::dependancySort($models);
+	foreach($models as $model)
+	{
+	    $sql .= $model->getDriver()->dropTable($model);
+	}
+
+	if ( !is_null($sql) && $confirm )
+	{
+	    self::execute($sql);
+	}
+	return $sql;
+    }
+
+
+    /**
+     * Models operation for syncing database structure with defined models
+     *
+     * If confirm is set, sql code will be executed
+     *
+     * @todo syncmodels
+     * @param boolean $confirm
+     * @return string
+     */
     public static function syncdb($confirm = false)
     {
 	$sql= null;
@@ -152,124 +402,13 @@ class DibiOrmController
 	return $sql;
     }
 
-    public static function sqlall()
-    {
-	$sql= null;
-	foreach( self::getModels() as $model)
-	{
-	    $sql .= $model->getDriver()->createTable($model);
-	}
-	return $sql;
-    }
-
-    public static function sqlclear($confirm)
-    {
-	$sql= null;
-	$models = array();
-
-	foreach( self::getModels() as $model)
-	{
-	    if ( $model->getConnection()->getDatabaseInfo()->hasTable($model->getTableName()) )
-	    {
-		$models[]= $model;
-	    }
-	}
-	self::dependancySort($models);
-	foreach($models as $model)
-	{
-	    $sql .= $model->getDriver()->dropTable($model);
-	}
-
-	if ( !is_null($sql) && $confirm )
-	{
-	    self::execute($sql);
-	}
-	return $sql;
-    }
-
-    protected static function execute($sql)
-    {
-	self::getConnection()->begin();
-	self::getConnection()->query($sql);
-	self::getConnection()->commit();
-    }
-
-    protected static function dependancySortReverse( &$list)
-    {
-	self::dependancySort($list);
-	$list= array_reverse($list);
-    }
-
-    protected static function dependancySort( &$list)
-    {
-	$_list= $list;
-	foreach($_list as $item)
-	{
-	    $min= null;
-	    foreach($item->getDependents() as $dependent)
-	    {
-		$min= ( is_null($min )) ? array_search($dependent, $_list) : min($min, array_search($dependent, $_list));
-	    }
-	    if (is_integer($min))
-	    {
-		if ( array_search($item, $_list) > $min)
-		{
-		    unset($list[array_search($item,$list)]);
-		    $list= self::insertArrayIndex($list, $item, $min);
-		}
-	    }
-	}
-    }
-
-    public static function queryAndLog()
-    {
-	$args= func_get_args();
-	self::getConnection()->query($args);
-	self::addSql(dibi::$sql); //log sql
-    }
-
-
-
-    protected static function insertArrayIndex($array, $new_element, $index)
-    {
-	$start = array_slice($array, 0, $index);
-	$end = array_slice($array, $index);
-	$start[] = $new_element;
-	return array_merge($start, $end);
-    }
-
-    public static function addSql($sql)
-    {
-	self::$sqlBuffer[]= $sql;
-    }
 
     /**
-     * @return array|boolean
+     * Getter which controls if models will use caching
+     * @return boolean
      */
-    public static function getSqlBufferAndClear()
-    {
-	$buffer= self::$sqlBuffer;
-	self::$sqlBuffer= array();
-	return (empty($buffer)) ? false : $buffer;
-    }
-
-
-    public static function enableModelCaching()
-    {
-	self::$modelCaching= true;
-    }
-
-
-    public static function disableModelCaching()
-    {
-	self::$modelCaching= false;
-    }
-
     public static function useModelCaching()
     {
 	return self::$modelCaching;
     }
-
-    
-
 }
