088343942f4f91e3b9dd97f6c57e9b6a00c0f413
Eduard Kracmar <kracmar@dannax.sk>
2010-02-18 13:52:12 +0100

    Added fshl's class that supports ANSI color codes

---
+++ libs/fshl/out/ANSI_UTF8_output.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * FastSHL                              | Universal Syntax HighLighter |
+ * ---------------------------------------------------------------------
+
+   Copyright (C) 2002-2005  Juraj 'hvge' Durech
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ * ---------------------------------------------------------------------
+ * HTML_UTF8_output.php		STANDARD HTML output module with UTF-8 encoding
+ */
+
+class ANSI_UTF8_output
+{
+	var $last_class;
+
+
+	function classToMarker($class)
+	{
+	    switch ($class)
+	    {
+		case 'sql-comment':
+		    return '%y';
+		
+		case 'sql-num':
+		    return '%R';
+
+		case 'sql-value':
+		    return '%b';
+
+		case 'sql-keyword1':
+		case 'sql-keyword2':
+		    return '%9%r';
+
+		case 'sql-keyword3':
+		    return '%9%b';
+
+
+		default:
+		    return $class;
+	    }
+	}
+
+	function HTML_UTF8_output()
+	{
+		$this->last_class = null;
+	}
+
+	function template ($word, $class)
+	{
+//              $word = htmlEntities($word, ENT_COMPAT, 'UTF-8');
+                $word = htmlSpecialChars($word, ENT_COMPAT);
+
+		if ($this->last_class == $class)
+		{
+			return $word;
+		}
+		else
+		{
+			if($this->last_class == null)
+			{
+				$this->last_class = $class;
+				return $this->classToMarker($class).$word;
+			}
+			$this->last_class = $class;
+			if($class == null) return "%n$word";
+			return "%n".$this->classToMarker($class).$word;
+		}
+	}
+
+	function template_end()
+	{
+		if($this->last_class != null) {
+			$this->last_class = null;
+			return "%n";
+		} else {
+			return null;
+		}
+	}
+
+	function keyword ($word, $class)
+	{
+//		$word = htmlEntities($word, ENT_COMPAT, 'UTF-8');
+                $word = htmlSpecialChars($word, ENT_COMPAT);
+
+		if ($this->last_class == $class)
+		{
+			return $word;
+		}
+		else
+		{
+			if($this->last_class == null)
+			{
+				$this->last_class = $class;
+				return $this->classToMarker($class).$word;
+			}
+			$this->last_class = $class;
+			if($class == null) return "%n$word";
+			return "%n".$this->classToMarker($class).$word;
+		}
+	}
+
+}
