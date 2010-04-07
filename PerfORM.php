17876b539909619d7431b1cc0fe1389b514d2dfc
Eduard Kracmar <kracmar@dannax.sk>
2010-04-07 10:42:07 +0200

    Default tablename converts camelcase model name to underscores

--- PerfORM.php
+++ PerfORM.php
@@ -650,7 +650,10 @@ abstract class PerfORM
     {
 	if ( is_null($this->tableName))
 	{
-	    $this->tableName= strtolower(get_class($this));
+	    $className= get_class($this);
+	    $className[0] = strtolower($className[0]);
+	    $func = create_function('$c', 'return "_" . strtolower($c[1]);');
+	    $this->tableName= preg_replace_callback('/([A-Z])/', $func, $className);
 	}
 	return is_null($this->prefix) ? $this->tableName : $this->prefix . $this->tableName ;
     }
